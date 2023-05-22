fb5fc00f83888f4d7c9c6a8392b9e1c722c441c2
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2023-05-22 11:30:16 +0100

    update roles and agent pool

--- components/logic-app/roles.tf
+++ components/logic-app/roles.tf
@@ -1,5 +1,5 @@
 resource "azurerm_role_assignment" "storage_table_contributor" {
-  scope                = azurerm_storage_table.finops_reporting_storage_table.id
+  scope                = module.finops_reporting_storage_account.storageaccount_id
   role_definition_name = "Storage Table Data Contributor"
   principal_id         = azurerm_logic_app_workflow.logic_app_workflow.identity[0].principal_id
 }

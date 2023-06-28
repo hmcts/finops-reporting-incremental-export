670cc6a9057cfab88d2ea210bfa1ca43e241cd0d
JusticeCarl <133108937+JusticeCarl@users.noreply.github.com>
2023-06-28 17:44:06 +0100

    added debug echos

--- .github/workflows/bash-scripts/balancesbyperiod.sh
+++ .github/workflows/bash-scripts/balancesbyperiod.sh
@@ -7,7 +7,7 @@ data_source="reservationchanges"
 
 # source file specific vars
 source_dir="${working_dir}"
-source_file_name="${data_source}-$(date +%y%m%d-%H%M%S).json"
+# source_file_name="${data_source}-$(date +%y%m%d-%H%M%S).json"
 # source_full_path="${source_dir}/${source_file_name}" # This is populated in the while loop for this script 
 
 # destination speciifc vars
@@ -23,22 +23,24 @@ billing_period=$(date +%Y%m)
 
 for filter in "${filter[@]}"
 do
-    export_name="Reservationcharges_${subscription_name}_${filter}_${billing_period}.json"
+
+    source_file_name="${data_source}_${subscription_name}_${filter}_${billing_period}.json"
     base_url="https://management.azure.com/providers/Microsoft.Billing/billingAccounts/${billing_account}/billingPeriods/'${billing_period}'/providers/Microsoft.Consumption/balances?&api-version=2023-03-01"
     # testing echos
     echo "---- INFO: The URL being used is:"
     echo $base_url
     echo "----"
     # base_url="https://management.azure.com/providers/Microsoft.Billing/billingAccounts/${billing_account}/billingPeriods/'${billing_period}'/providers/Microsoft.Consumption/balances?&api-version=2023-03-01"
-    az rest --method get --url ${base_url} > ${working_dir}/${export_name}
+    source_full_path="${source_dir}/${source_file_name}"
+    az rest --method get --url ${base_url} > ${source_full_path}
     # now upload to storage
 
     if [[ -f .github/workflows/bash-scripts/storage_account_upload.sh ]]
         then
             source .github/workflows/bash-scripts/storage_account_upload.sh
-            destination_full_path="/${destination_path}/${source_filename}"
+            destination_full_path="/${destination_path}/${source_file_name}"
             echo "upload to Storage Account: "${storage_account_name}" container:" ${container_name} " Path:"${destination_full_path}
-            destination_full_path="/${destination_path}/${source_filename}"
+            destination_full_path="/${destination_path}/${source_file_name}"
             Upload_to_storage
         else
             echo "ERROR: cant find .github/workflows/bash-scripts/storage_account_upload.sh current path:"

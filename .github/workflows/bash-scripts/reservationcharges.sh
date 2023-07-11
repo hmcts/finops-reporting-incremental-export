33368db487024f2809faaa97a902c280796b96f8
JusticeCarl <133108937+JusticeCarl@users.noreply.github.com>
2023-07-11 15:42:35 +0100

    fixed typo in data_source name

--- .github/workflows/bash-scripts/reservationcharges.sh
+++ .github/workflows/bash-scripts/reservationcharges.sh
@@ -3,7 +3,7 @@
 # script specific variables
 working_dir=$(mktemp -d)
 IFS="|"
-data_source="reservationchanges"
+data_source="reservationcharges"
 
 # source file specific vars
 source_dir="${working_dir}"

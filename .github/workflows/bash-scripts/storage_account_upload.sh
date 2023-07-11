82d6bfc876e3a512c8a3c80ea2d18c13258c1271
JusticeCarl <133108937+JusticeCarl@users.noreply.github.com>
2023-07-11 13:54:30 +0100

    fixed typo of (

--- .github/workflows/bash-scripts/storage_account_upload.sh
+++ .github/workflows/bash-scripts/storage_account_upload.sh
@@ -37,7 +37,7 @@ function Upload_to_storage(){
     echo "INFO: Storage Account name is:" ${storage_account_name}
     echo "INFO: Resource Group name is :" ${resource_group}
     echo "INFO: Destination path is: " ${destination_full_path}
-    az storage fs directory upload --file-system ${container_name} --account-name ${storage_account_name} --account-key $(sa_key} --destination-path ${destination_full_path} --source ${source_full_path} --recursive
+    az storage fs directory upload --file-system ${container_name} --account-name ${storage_account_name} --account-key ${sa_key} --destination-path ${destination_full_path} --source ${source_full_path} --recursive
     
     if [[ $? -ne 0 ]]
         then

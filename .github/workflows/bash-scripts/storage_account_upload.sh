631e7a68b123c2262c8df3b0ef74c1d684906aa5
JusticeCarl <133108937+JusticeCarl@users.noreply.github.com>
2023-07-11 15:37:28 +0100

    replaced az dirtectory upload with file upload

--- .github/workflows/bash-scripts/storage_account_upload.sh
+++ .github/workflows/bash-scripts/storage_account_upload.sh
@@ -37,8 +37,8 @@ function Upload_to_storage(){
     echo "INFO: Storage Account name is:" ${storage_account_name}
     echo "INFO: Resource Group name is :" ${resource_group}
     echo "INFO: Destination path is: " ${destination_full_path}
-    az storage fs directory upload --file-system ${container_name} --account-name ${storage_account_name} --account-key ${sa_key} --destination-path ${destination_full_path} --source ${source_full_path} --recursive
-    
+    # az storage fs directory upload --file-system ${container_name} --account-name ${storage_account_name} --account-key ${sa_key} --destination-path ${destination_full_path} --source ${source_full_path} --recursive
+    az storage fs file upload --file-system ${container_name} --account-name ${storage_account_name} --account-key ${sa_key} --path ${destination_full_path} --source ${source_full_path} --overwrite true
     if [[ $? -ne 0 ]]
         then
             echo "ERROR: upload failed!"

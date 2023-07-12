9e7520c04c8735b0c61c4009e0c39849fed32de9
JusticeCarl <133108937+JusticeCarl@users.noreply.github.com>
2023-07-12 15:59:16 +0100

    sorted quotes

--- .github/workflows/bash-scripts/reservationcharges.sh
+++ .github/workflows/bash-scripts/reservationcharges.sh
@@ -40,8 +40,9 @@ fi
 
 if [[ -f .github/workflows/bash-scripts/storage_account_upload.sh ]]
     then
-        echo "upload to Storage Account: "${storage_account_name}" container:" ${container_name} " Path:"${destination_full_path}
+        
         destination_full_path="/${destination_path}/${source_filename}"
+        echo "upload to Storage Account: "${storage_account_name}" container:" ${container_name} " Path:"${destination_full_path}
         source .github/workflows/bash-scripts/storage_account_upload.sh
         Upload_to_storage
     else

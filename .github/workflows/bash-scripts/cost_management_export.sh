4f454eb9ba41ec8045444d022c865b8bf6ab8885
JusticeCarl <133108937+JusticeCarl@users.noreply.github.com>
2023-07-17 15:49:02 +0100

    removed section which adds timeperiod as conflicting with schedule

--- .github/workflows/bash-scripts/cost_management_export.sh
+++ .github/workflows/bash-scripts/cost_management_export.sh
@@ -10,10 +10,6 @@ exports_path="TEST_EXPORTS_FOLDER"
 data_source=cost_management_export
 resource_id="/subscriptions/${subscription_id}/resourceGroups/${resource_group}/providers/Microsoft.Storage/storageAccounts/${storage_account_name}"
 export_name="${data_source}-$(date +%Y-%m-%d-%H%M)"
-# we need yesterdays date 
-# start_period="2023-07-01T00:00:00Z"
-# end_period="3000-01-01T00:00:00Z"
-
 start_period=$(date  -d '1 day ago' +"%Y-%m-%d"T00:00:00+00:00Z)
 end_period=$(date  -d '1 day ago' +"%Y-%m-%d"T23:59:59+00:00Z)
 
@@ -28,7 +24,7 @@ jq \
   --arg f ${start_period} \
   --arg e ${end_period} \
   --arg p ${exports_path}\
-  '.properties.deliveryInfo.destination.resourceId = $r | .properties.deliveryInfo.destination.container = $c | .properties.deliveryInfo.destination.rootFolderPath = $p | .properties.definition.timePeriod.from = $f | .properties.definition.timePeriod.to = $e' .github/workflows/bash-scripts/cost_management_export.json > ./${post_file_name}
+  '.properties.deliveryInfo.destination.resourceId = $r | .properties.deliveryInfo.destination.container = $c | .properties.deliveryInfo.destination.rootFolderPath = $p .github/workflows/bash-scripts/cost_management_export.json > ./${post_file_name}
 
 cat ./${post_file_name}
 
